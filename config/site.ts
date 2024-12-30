458ae54b8787e1ea0bc203350d3ceb1c1a356b1c
Benjy <123623302+brrock@users.noreply.github.com>
2024-12-30 15:55:07 +0000

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
 	name: "Benjy Ross",
 	url: "https://benjyross.xyz",
 	description:
-		"Nextjs 15 portfolio using velite, tailwind and shadcn, and some dev knowledge",
+		"My personal website",
 	author: "Benjy Ross",
 	links: {
 		github: "https://github.com/brrock",
